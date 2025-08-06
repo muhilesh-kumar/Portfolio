06b96bfb0c53c81fcdcb0c1334bac7debea70d2d
muhilesh-kumar <muhileshkumarwork@gmail.com>
2025-08-06 18:24:50 +0530

    Updated portfolio with latest changes

--- start/start/settings.py
+++ start/start/settings.py
@@ -34,8 +34,8 @@ SECRET_KEY = 'django-insecure-#ni318mjfgvb1l!*8@5bo#=^62xkk!^k$7wcxa7%$&%+1bd(7-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ['portfolio-2-omqg.onrender.com']
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ['portfolio-2-omqg.onrender.com']
+# ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 
 
